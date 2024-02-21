9c03fe8a6dee9a1493d6a6d783b01e868e7baca7
Alex Figliolia <alexfigliolia@gmail.com>
2024-02-21 12:44:43 -0800

    Generic debouncer

---
+++ src/Generics/Debouncer.ts
@@ -0,0 +1,28 @@
+import type { Callback } from "Types/Generics";
+
+export class Debouncer<T extends Callback> {
+  public FN: T;
+  threshold: number;
+  private debouncer: ReturnType<typeof setTimeout> | null = null;
+  constructor(FN: T, threshold: number) {
+    this.FN = FN;
+    this.threshold = threshold;
+  }
+
+  public execute(...args: Parameters<T>) {
+    if (this.debouncer) {
+      this.clear();
+    }
+    this.debouncer = setTimeout(() => {
+      void this.FN(...args);
+      this.clear();
+    }, this.threshold);
+  }
+
+  public clear() {
+    if (this.debouncer) {
+      clearTimeout(this.debouncer);
+      this.debouncer = null;
+    }
+  }
+}
