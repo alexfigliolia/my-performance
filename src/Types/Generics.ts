53419e5a7ad150ae0edd61ef33c39584b7d277e2
Alex Figliolia <alexfigliolia@gmail.com>
2024-02-21 12:45:02 -0800

    More generics

--- src/Types/Generics.ts
+++ src/Types/Generics.ts
@@ -2,4 +2,6 @@ export type PropertyKeys<T, M> = {
   [K in keyof T]: T[K] extends M ? K : never;
 }[keyof T];
 
-export type Callback<T = void> = () => T | Promise<T>;
+export type Callback<T = void, A extends any[] = never[]> = (
+  ...args: A
+) => T | Promise<T>;
