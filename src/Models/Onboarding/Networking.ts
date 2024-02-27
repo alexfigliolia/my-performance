da5df4b5ce612d08031755b4a56ad9995e711d80
Alex Figliolia <alexfigliolia@gmail.com>
2024-02-26 21:54:49 -0800

    Close sse connection after installation setup

--- src/Models/Onboarding/Networking.ts
+++ src/Models/Onboarding/Networking.ts
@@ -6,7 +6,7 @@ import {
   createGithubAccount,
   GQLRequest,
   GQLSubscription,
-  installationSetupSubscription,
+  installationSetupStream,
   Platform,
 } from "GQL";
 import type { IOnboarding, ISubscription } from "./types";
@@ -16,7 +16,7 @@ export class Networking {
   Subscription: ISubscription;
   constructor(state: IOnboarding) {
     this.state = state;
-    this.Subscription = new GQLSubscription(installationSetupSubscription, {
+    this.Subscription = new GQLSubscription(installationSetupStream, {
       platform: Platform.Github,
       installation_id: state.installation_id,
     }) as ISubscription;
@@ -26,9 +26,9 @@ export class Networking {
     this.Subscription.open();
     return new Promise((resolve, reject) => {
       this.Subscription.onData(data => {
-        const { id } = data.data?.installationSetup ?? {};
+        const { id } = data.data?.installationSetupStream ?? {};
         if (typeof id === "number") {
-          this.Subscription.close();
+          this.Subscription.closeAll();
           void this.createUserAccount().then(resolve);
         }
       });
@@ -37,7 +37,7 @@ export class Networking {
   }
 
   public unsubscribe() {
-    return this.Subscription.close();
+    return this.Subscription.closeAll();
   }
 
   private createUserAccount() {
