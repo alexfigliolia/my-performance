2f7ab7b7debef769121dea95c2291b77ef89c0d7
Alex Figliolia <alexfigliolia@gmail.com>
2024-03-13 10:42:18 -0700

    React bs

--- src/Components/Teammate/Output/index.tsx
+++ src/Components/Teammate/Output/index.tsx
@@ -1,32 +1,25 @@
-import React, { Component } from "react";
+import React, { memo, useMemo } from "react";
 import { ProgressRing } from "Components/Graphs";
 import "./styles.scss";
 
-export class Output extends Component<Props> {
-  gradientID: string;
-  constructor(props: Props) {
-    super(props);
-    this.gradientID = `${this.props.id}Output`;
-  }
-
-  public override shouldComponentUpdate({ progress }: Props) {
-    return progress !== this.props.progress;
-  }
-
-  public override render() {
-    const { progress, color1, color2 } = this.props;
-    return (
-      <ProgressRing
-        progress={progress}
-        ringStyle={{ "--progress-stroke": `url(#${this.gradientID})` }}>
-        <linearGradient id={this.gradientID} x1="1" x2="0" y1="0" y2="1">
-          <stop stopColor={color1} offset="0" />
-          <stop stopColor={color2} offset="1" />
-        </linearGradient>
-      </ProgressRing>
-    );
-  }
-}
+export const Output = memo(function Output({
+  id,
+  color1,
+  color2,
+  progress,
+}: Props) {
+  const gradientID = useMemo(() => `${id}Output`, [id]);
+  return (
+    <ProgressRing
+      progress={progress}
+      ringStyle={{ "--progress-stroke": `url(#${gradientID})` }}>
+      <linearGradient id={gradientID} x1="1" x2="0" y1="0" y2="1">
+        <stop stopColor={color1} offset="0" />
+        <stop stopColor={color2} offset="1" />
+      </linearGradient>
+    </ProgressRing>
+  );
+});
 
 interface Props {
   id: string;
--- src/Components/Teammate/index.tsx
+++ src/Components/Teammate/index.tsx
@@ -7,33 +7,36 @@ import { Output } from "./Output";
 import { Stats } from "./Stats";
 import "./styles.scss";
 
-export const Teammate = memo(
-  function Teammate({ name, lines, commits, linesPerMonth }: OverallStats) {
-    const output = useTeam(state =>
-      Math.round((lines * 100) / state.totalLines),
-    );
-    const [color1, color2] = Controller.getColors(output);
-    const ID = useMemo(() => name.replaceAll(" ", ""), [name]);
+export const Teammate = memo(function Teammate({
+  name,
+  lines,
+  commits,
+  linesPerMonth,
+}: OverallStats) {
+  console.log({ name, lines });
+  const totalLines = useTeam(state => state.totalLines);
+  const output = useMemo(
+    () => Math.floor((lines * 100) / totalLines),
+    [lines, totalLines],
+  );
+  const [color1, color2] = Controller.getColors(output);
+  const ID = useMemo(() => name.replaceAll(" ", ""), [name]);
 
-    return (
-      <ListItemTile className="teammate">
-        <div className="row">
-          <Output id={ID} color1={color1} color2={color2} progress={output} />
-          <Stats
-            id={ID}
-            name={name}
-            lines={lines}
-            color1={color1}
-            color2={color2}
-            commits={commits}
-            recentPullRequests={40}
-            linesPerMonth={linesPerMonth}
-          />
-        </div>
-      </ListItemTile>
-    );
-  },
-  (pp, np) => {
-    return pp.id === np.id;
-  },
-);
+  return (
+    <ListItemTile className="teammate">
+      <div className="row">
+        <Output id={ID} color1={color1} color2={color2} progress={output} />
+        <Stats
+          id={ID}
+          name={name}
+          lines={lines}
+          color1={color1}
+          color2={color2}
+          commits={commits}
+          recentPullRequests={40}
+          linesPerMonth={linesPerMonth}
+        />
+      </div>
+    </ListItemTile>
+  );
+});
